ede6430f659cd57d34608bf54137c046ecd855d3
Nihal-a <nihalavulan1@gmail.com>
2021-07-19 09:35:11 +0530

    admin pages designed

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('admin/home',{admin:true})
+  res.render('admin/home',{adminH:true})
 });
+
+router.get('/employers',(req,res)=>{
+    res.render('admin/employers',{adminH:true})
+})
+
+router.get('/users',(req,res)=>{
+    res.render('admin/users',{adminH:true})
+})
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('user/home',{user:true})
+  res.render('user/home',{userH:true})
 });
 
 module.exports = router;
