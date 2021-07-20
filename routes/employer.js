0f36e02fb2a74c249fd66c534e9328099b55aaa8
Nihal-a <nihalavulan1@gmail.com>
2021-07-20 10:15:18 +0530

    employer basic  folder structures and route created

--- routes/employer.js
+++ routes/employer.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('employer/home',{employerH:true})
 });
 
+router.get('/jobs',(req,res)=>{
+  res.render('employer/jobs',{employerH:true})
+})
+
+router.get('/resume-requests',(req,res)=>{
+  res.render('employer/resume-requests',{employerH:true})
+})
+
+router.get('/approved-resumes',(req,res)=>{
+  res.render('employer/approved-resumes',{employerH:true})
+})
+
+router.get('/rejected-resumes',(req,res)=>{
+  res.render('employer/rejected-resumes',{employerH:true})
+})
+
+router.get('/logout',(req,res)=>{
+  
+})
 module.exports = router;
