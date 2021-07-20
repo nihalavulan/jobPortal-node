aa06947619645ba23a8ab8a343ed4a33d8d078f6
Nihal-a <nihalavulan1@gmail.com>
2021-07-20 09:58:55 +0530

    User layouts and home page done

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('user/home',{userH:true})
+  res.render('user/home',{userH:true,userF:true})
 });
 
 module.exports = router;
